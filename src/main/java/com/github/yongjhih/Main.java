ca6252d95ac7680703fde8ecdcb095b7ba9d5e16
Wendly.Li <wendly777@gmail.com>
2015-03-25 12:59:37 +0800

    Update test case

--- src/main/java/com/github/yongjhih/Main.java
+++ src/main/java/com/github/yongjhih/Main.java
@@ -4,8 +4,8 @@ import rx.schedulers.*;
 import rx.Observable;
 import rx.functions.*;
 import rx.observables.*;
-import rx.util.*;
 import rx.internal.operators.*;
+import rx.subjects.*;
 import java.util.concurrent.*;
 
 public class Main {
@@ -19,9 +19,25 @@ public class Main {
             }).subscribe(System.out::println);
         */
 
-        Observable.range(1, 10)
+        Subject<Integer, Integer> mBus = PublishSubject.create();
+        mBus.onNext(1);
+        mBus.asObservable()
             .lift(new OperatorTick<Integer>(1, TimeUnit.SECONDS))
             .subscribe(System.out::println, e -> {}, () -> System.out.println("onCompleted"));
+
+        mBus.onNext(2);
+        mBus.onNext(4);
+        mBus.onNext(6);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        mBus.onNext(1);
+        mBus.onNext(3);
+        mBus.onNext(5);
         while (true);
     }
 }
